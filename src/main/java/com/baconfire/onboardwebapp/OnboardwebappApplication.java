5b2ca592fb7c52ac7dcdbb2d19339e074a2fd069
Fenting Lin <fen@pdx.edu>
2020-03-17 16:41:12 -0400

    init commit

---
+++ src/main/java/com/baconfire/onboardwebapp/OnboardwebappApplication.java
@@ -0,0 +1,13 @@
+package com.baconfire.onboardwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardwebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baconfire/onboardwebapp/OnboardwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baconfire.onboardwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardwebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
