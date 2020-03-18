9b5497efde16e86779122b4850c24ec7ab392fdc
Fenting Lin <fen@pdx.edu>
2020-03-17 20:04:43 -0400

    change phone date type

--- src/main/java/com/baconfire/onboardwebapp/domain/Person.java
+++ src/main/java/com/baconfire/onboardwebapp/domain/Person.java
@@ -1,5 +1,13 @@
 package com.baconfire.onboardwebapp.domain;
+import lombok.*;
+
 import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 
 @Entity
 @Table(name="Person")
@@ -23,10 +31,10 @@ public class Person {
     private String Email;
 
     @Column(name="CellPhone")
-    private long CellPhone;
+    private String CellPhone;
 
     @Column(name="AlternatePhone")
-    private long AlternatePhone;
+    private String AlternatePhone;
 
     @Column(name="Gender")
     private String Gender;
@@ -36,100 +44,4 @@ public class Person {
 
     @Column(name="DOB")
     private String DOB;
-
-    public Person() {
-    }
-
-    public Person(int id, String firstName, String lastName, String middleName, String email, long cellPhone, long alternatePhone, String gender, String SSN, String DOB) {
-        Id = id;
-        FirstName = firstName;
-        LastName = lastName;
-        MiddleName = middleName;
-        Email = email;
-        CellPhone = cellPhone;
-        AlternatePhone = alternatePhone;
-        Gender = gender;
-        this.SSN = SSN;
-        this.DOB = DOB;
-    }
-
-    public int getId() {
-        return Id;
-    }
-
-    public void setId(int id) {
-        Id = id;
-    }
-
-    public String getFirstName() {
-        return FirstName;
-    }
-
-    public void setFirstName(String firstName) {
-        FirstName = firstName;
-    }
-
-    public String getLastName() {
-        return LastName;
-    }
-
-    public void setLastName(String lastName) {
-        LastName = lastName;
-    }
-
-    public String getMiddleName() {
-        return MiddleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        MiddleName = middleName;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public long getCellPhone() {
-        return CellPhone;
-    }
-
-    public void setCellPhone(long cellPhone) {
-        CellPhone = cellPhone;
-    }
-
-    public long getAlternatePhone() {
-        return AlternatePhone;
-    }
-
-    public void setAlternatePhone(long alternatePhone) {
-        AlternatePhone = alternatePhone;
-    }
-
-    public String getGender() {
-        return Gender;
-    }
-
-    public void setGender(String gender) {
-        Gender = gender;
-    }
-
-    public String getSSN() {
-        return SSN;
-    }
-
-    public void setSSN(String SSN) {
-        this.SSN = SSN;
-    }
-
-    public String getDOB() {
-        return DOB;
-    }
-
-    public void setDOB(String DOB) {
-        this.DOB = DOB;
-    }
 }
