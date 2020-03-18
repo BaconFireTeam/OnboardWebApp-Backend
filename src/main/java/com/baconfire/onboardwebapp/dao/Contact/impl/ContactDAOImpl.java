7d57691fff3ef910804e21d2e9f855b315dcee53
QINYA <QYZ@QINYAs-MacBook-Air.local>
2020-03-18 13:25:22 -0400

    DAO complete

---
+++ src/main/java/com/baconfire/onboardwebapp/dao/Contact/impl/ContactDAOImpl.java
@@ -0,0 +1,10 @@
+package com.baconfire.onboardwebapp.dao.Contact.impl;
+
+import com.baconfire.onboardwebapp.dao.AbstractHibernateDAO;
+import com.baconfire.onboardwebapp.dao.ApplicationWorkFlow.ApplicationWorkFlowDAO;
+import com.baconfire.onboardwebapp.dao.Contact.ContactDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ContactDAOImpl extends AbstractHibernateDAO implements ContactDAO {
+}
---
+++ src/main/java/com/baconfire/onboardwebapp/dao/RolePermission/impl/RolePermissionDAOImpl.java
@@ -0,0 +1,9 @@
+package com.baconfire.onboardwebapp.dao.RolePermission.impl;
+
+import com.baconfire.onboardwebapp.dao.AbstractHibernateDAO;
+import com.baconfire.onboardwebapp.dao.RolePermission.RolePermissionDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RolePermissionDAOImpl extends AbstractHibernateDAO implements RolePermissionDAO {
+}
