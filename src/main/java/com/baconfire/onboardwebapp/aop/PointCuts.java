b87efbb26227f9aafaf3d3b3ef8e2b75f0b3c23d
edwardng827 <edwardng827@gmail.com>
2020-03-25 17:00:19 -0400

    uncommented auth

---
+++ src/main/java/com/baconfire/onboardwebapp/aop/PointCuts.java
@@ -0,0 +1,15 @@
+//package com.baconfire.onboardwebapp.aop;
+//
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.annotation.Pointcut;
+//import org.springframework.stereotype.Component;
+//
+//@Aspect
+//@Component
+//public class PointCuts {
+//
+//    @Pointcut("within(com.baconfire.onboardwebapp.controller.HRController)")
+//    public void checkHrNull(){
+//
+//    }
+//}
