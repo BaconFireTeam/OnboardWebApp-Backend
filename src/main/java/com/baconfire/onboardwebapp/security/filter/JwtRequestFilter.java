01339efbf61f3a2765289ffdd66a9a5aa305cc74
AthleticCorgi <peterli342@gmail.com>
2020-03-28 22:19:07 -0400

    further clean up JwtUtil and filter

--- src/main/java/com/baconfire/onboardwebapp/security/filter/JwtRequestFilter.java
+++ src/main/java/com/baconfire/onboardwebapp/security/filter/JwtRequestFilter.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.baconfire.onboardwebapp.security.filter;
 
 import java.io.IOException;
@@ -9,10 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
--- src/main/java/com/baconfire/onboardwebapp/security/util/JwtUtil.java
+++ src/main/java/com/baconfire/onboardwebapp/security/util/JwtUtil.java
@@ -1,24 +1,15 @@
 package com.baconfire.onboardwebapp.security.util;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Function;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 
-/**
- * @author Ian Li
- * this util class helps generate, validate jwt and helps extract information from jwt
- *
- */
 @Service
 public class JwtUtil {
 
@@ -55,20 +46,7 @@ public class JwtUtil {
 		return extractExpiration(token).before(new Date());
 	}
 
-	public String generateToken(UserDetails userDetails) {
-		Map<String, Object> claims = new HashMap<>();
-		return createToken(claims, userDetails.getUsername());
-	}
-
-	private String createToken(Map<String, Object> claims, String subject) {
-
-		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-				.signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
-	}
-
 	public Boolean validateToken(String token) {
 		return (!isTokenExpired(token));
 	}
 }
-
